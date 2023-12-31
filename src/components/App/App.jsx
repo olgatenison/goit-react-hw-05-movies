1d6a384f02067c59818826711afd121da32b4fc0
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-12-31 17:31:13 +0200

    Layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Layout } from 'components/Layout/Layout';
 
 const Cast = lazy(() => import('components/Cast/Cast'));
 const Reviews = lazy(() => import('components/Reviews/Reviews'));
